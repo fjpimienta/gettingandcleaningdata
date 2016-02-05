2d0e8abd914bc4ec2878b1337a9a19e0dab5d81b
fjpimienta <fjpimienta@gmail.com>
2016-02-04 22:17:41 -0600

    Update 2016  02 14

--- run_analysis.R
+++ run_analysis.R
@@ -1,103 +1,101 @@
-##install.packages("data.table")
-##install.packages("reshape2")
-##setwd("C:/Users/SIAVHSA/Documents/R/Datos/")
-##path <- getwd()
-library(data.table)
+##install.packages("plyr")
+##install.packages("dplyr")
+library(plyr)
+library(dplyr)
 library(reshape2)
-fileUrl <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
-f <- 'Dataset.zip'
-download.file(fileUrl,f)
+library(stringr)
+
+## read in the features
+setwd("/Users/familiaperezfuentes/GitHub/Data/")
+path <- getwd()
 d <- 'UCI HAR Dataset'
-unzip(f)
+features <- read.table(file.path(path, d, 'features.txt'))
+## read in the test data
+test_x <- read.table(file.path(path, d, 'test', 'X_test.txt'))
+test_y <- read.table(file.path(path, d, 'test', 'y_test.txt'))
+test_subjects <- read.table(file.path(path, d, 'test', 'subject_test.txt'))
+train_x <- read.table(file.path(path, d, 'train','X_train.txt'))
+train_y <- read.table(file.path(path, d, 'train','y_train.txt'))
+train_subjects <- read.table(file.path(path, d, 'train','subject_train.txt'))
 
-##Read Subjects
-dtsubject_train <- data.table(read.table(file.path(path, d, 'train', 'subject_train.txt')))
-dtsubject_test <- data.table(read.table(file.path(path, d, 'test', 'subject_test.txt')))
-dtsubject <- rbind(dtsubject_train, dtsubject_test)
-names(dtsubject) <- c('Subject')
-remove(dtsubject_train,dtsubject_test)
+## Combine the dates 
+combined_data <- rbind(test_x, train_x)
+combined_y <- rbind(test_y, train_y)
+combined_subjects <- rbind(test_subjects, train_subjects)
 
-##Read activities
-dtY_train <- data.table(read.table(file.path(path, d, 'train','Y_train.txt')))
-dtY_test <- data.table(read.table(file.path(path,d,'test','Y_test.txt')))
-dtAct <- rbind(dtY_train,dtY_test)
-names(dtAct) <- c('Activity')
-remove(dtY_train,dtY_test)
+## apply the features V2 column as the headers for the combined_data data frame
+## clean up the names first to remove hyphens and parentheses
+listOfNames <- as.character(features$V2)
+listOfNames <- make.names(listOfNames, unique=TRUE)
+names(combined_data) <- listOfNames
 
-##Combine subject and activity
-dtsubject <- cbind(dtsubject,dtAct)
-remove(dtAct)
+## add combined_y and combined_subjects as the first columns in the combined_data data frame
+combined_data <- cbind(combined_y, combined_data)
+names(combined_subjects) = c('subjects')
+combined_data <- cbind(combined_subjects, combined_data)
 
-##Read feature data
-dtX_train <- data.table(read.table(file.path(path,d,'train','X_train.txt')))
-dtX_test <- data.table(read.table(file.path(path,d,'test','X_test.txt')))
-dt <- rbind(dtX_train,dtX_test)
-remove(dtX_train,dtX_test)
+## extract only the measurements on the mean and standard deviation for each measurement
+sub_data <- select(combined_data, subjects, V1, contains(".mean."), contains(".std."))
 
-#merge into one table subject/activity/feature
-dt <- cbind(dtsubject,dt)
+## convert the subject and activities columns to factors
+sub_data$subjects <- as.factor(sub_data$subjects)
+sub_data$V1 <- as.factor(sub_data$V1)
 
-##set key to subject/activity
-setkey(dt,Subject,Activity)
-remove(dtsubject)
+## use descriptive activity names to name the activites in the data set
+sub_data$V1 <- mapvalues(sub_data$V1, from = c("1", "2", "3", "4", "5", "6"), 
+                         to = c("Walking", "WalkingUpStairs", "WalkingDownStairs", "Sitting", "Standing", "Lying"))
 
-#read feature names, get only std and mean features
-dtfeatures <- data.table(read.table(file.path(path,d,'features.txt'))) 
-names(dtfeatures) <- c('ftNum','ftName')
-dtfeatures <- dtfeatures[grepl("mean\\(\\)|std\\(\\)",ftName)]
-dtfeatures$ftCode <- paste('V', dtfeatures$ftNum, sep = "")
+## appropriately label the data set with descriptive variable names
+## (See codebook.md for an explanation of the variable names.)
+names(sub_data) <- str_replace_all(names(sub_data), "[.][.]", "")
+names(sub_data) <- str_replace_all(names(sub_data), "BodyBody", "Body")
+names(sub_data) <- str_replace_all(names(sub_data), "tBody", "Body")
+names(sub_data) <- str_replace_all(names(sub_data), "fBody", "FFTBody")
+names(sub_data) <- str_replace_all(names(sub_data), "tGravity", "Gravity")
+names(sub_data) <- str_replace_all(names(sub_data), "fGravity", "FFTGravity")
+names(sub_data) <- str_replace_all(names(sub_data), "Acc", "Acceleration")
+names(sub_data) <- str_replace_all(names(sub_data), "Gyro", "AngularVelocity")
+names(sub_data) <- str_replace_all(names(sub_data), "Mag", "Magnitude")
+for(i in 3:68) {if (str_detect(names(sub_data)[i], "[.]std")) 
+{names(sub_data)[i] <- paste0("StandardDeviation", str_replace(names(sub_data)[i], "[.]std", ""))}}
+for(i in 3:68) {if (str_detect(names(sub_data)[i], "[.]mean")) 
+{names(sub_data)[i] <- paste0("Mean", str_replace(names(sub_data)[i], "[.]mean", ""))}}
+names(sub_data) <- str_replace_all(names(sub_data), "[.]X", "XAxis")
+names(sub_data) <- str_replace_all(names(sub_data), "[.]Y", "YAxis")
+names(sub_data) <- str_replace_all(names(sub_data), "[.]Z", "ZAxis")
 
-#select only the filtered features (with=FALSE to dynamically pick cols)
-dt <- dt[,c(key(dt), dtfeatures$ftCode),with=F]
+## from the data set in step 4, create a second, independent tidy data set 
+## Use a split/apply/combine method. First, split the data by the subject and activity factors.
+split_set <- split(select(sub_data, 3:68), list(sub_data$subjects, sub_data$V1))
+## Next, use lapply to iterate over each item in the resulting list, and use apply to calculate the mean of each column.
+mean_set <- lapply(split_set, function(x) apply(x, 2, mean, na.rm=TRUE))
+## The output from lapply is a list. Convert this back to a data frame.
+tidy_set <- data.frame(t(sapply(mean_set,c)))
 
-##read activity names
-dtActNames <- data.table(read.table(file.path(path, d, 'activity_labels.txt')))
-names(dtActNames) <- c('Activity','ActivityName')
-dt <- merge(dt,dtActNames,by='Activity')
-remove(dtActNames)
+## The subject and activity factors are still combined, and are now row names instead of columns. Split them 
+## using strsplit, then add them to a separate data frame that can be combined with the tidy data set using cbind.
+factors <- data.frame(t(sapply(strsplit(rownames(tidy_set), "[.]"),c)))
+tidy_set <- cbind(factors, tidy_set)
 
-##add activityname as a key
-setkey(dt,Subject,Activity,ActivityName)
+## Give the subject and activity columns friendly names, and convert them to factors.
+tidy_set <- dplyr::rename(tidy_set,TestSubject = X1, Activity = X2)
+tidy_set$TestSubject <- as.factor(tidy_set$TestSubject)
+tidy_set$Activity <- as.factor(tidy_set$Activity)
+rownames(tidy_set) <- NULL
 
-##reshape data
-dt <- melt(dt, key(dt), variable.name='ftCode',value.name='ftValue')
-dt$Activity <- NULL
+## DATA VERIFICATION - "manually" generate a couple test variables to verify that the calculated average values are correct.
+## pull all of the data for subject = 1, activity = walking, variable = tBodyAcc.mean...X
+test_set <- select(filter(sub_data, V1=="Walking" & subjects==1), MeanBodyAccelerationXAxis)
+## calculate the mean, and compare it to the same calculation from the result set.
+tidy_set_val <- select(filter(tidy_set, TestSubject==1 & Activity=="Walking"), MeanBodyAccelerationXAxis)$MeanBodyAccelerationXAxis
+result <- all.equal(mean(test_set$MeanBodyAccelerationXAxis), tidy_set_val)
+print("Data calculation verification--TRUE indicates the verification passed:")
+print(result)
 
-##merge in ftName
-dt <- merge(dt,dtfeatures,by='ftCode')
-setkey(dt,Subject,ActivityName,ftName)
+## verification, with data from the middle of the matrix
+test_set <- select(filter(sub_data, V1=="Sitting" & subjects==5), StandardDeviationFFTBodyAccelerationXAxis)
+tidy_set_val <- select(filter(tidy_set, TestSubject==5 & Activity=="Sitting"), StandardDeviationFFTBodyAccelerationXAxis)$StandardDeviationFFTBodyAccelerationXAxis
+result <- all.equal(mean(test_set$StandardDeviationFFTBodyAccelerationXAxis), tidy_set_val)
+print(result)
 
-dtTidy <- dt[,.(ftAvg=mean(ftValue)),by=key(dt)]
-
-##start seperating out featName column to seperate columns
-#ftDomain: TIME FREQ
-dtTidy$ftDomain[grepl('^t',dtTidy$ftName)] <- 'Time'
-dtTidy$ftDomain[grepl('^f',dtTidy$ftName)] <- 'Freq'
-
-##ftInstrment:  Accelerometer Gyroscope
-dtTidy$ftInstrment[grepl('Acc',dtTidy$ftName)] <- 'Accelerometer'
-dtTidy$ftInstrment[grepl('Gyro',dtTidy$ftName)] <- 'Gyroscope'
-
-##ftAcceleration:  Body Gravity
-dtTidy$ftAcceleration[grepl('BodyAcc',dtTidy$ftName)] <- 'Body'
-dtTidy$ftAcceleration[grepl('GravityAcc',dtTidy$ftName)] <- 'Gravity'
-
-##ftStatVariable:  mean std
-dtTidy$ftStatVariable[grepl('mean()',dtTidy$ftName)] <- 'mean'
-dtTidy$ftStatVariable[grepl('std()',dtTidy$ftName)] <- 'std'
-
-##ftJerk: Y
-dtTidy$ftJerk[grepl('Jerk', dtTidy$ftName)] <- 'Y'
-
-##ftMagnitude: Y
-dtTidy$ftMagnitude[grepl('Mag', dtTidy$ftName)] <- 'Y'
-
-##ftAxis:  X Y Z
-dtTidy$ftAxis[grepl('-X', dtTidy$ftName)] <- 'X'
-dtTidy$ftAxis[grepl('-Y', dtTidy$ftName)] <- 'Y'
-dtTidy$ftAxis[grepl('-Z', dtTidy$ftName)] <- 'Z'
-
-dtTidy$ftName <- NULL
-
-##Write table
-write.table(dtTidy, file.path(path, 'tidy.txt'), row.names=FALSE)
+write.table(tidy_set, "tidy_data_set.txt", row.names=FALSE)
